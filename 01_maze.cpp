8ed5dbd7d9455c2b1fde0ca4146ccc14f01b2731
studiosuy <38967583+studiosuy@users.noreply.github.com>
2018-05-04 23:43:38 +0900

    Update 01_maze.cpp

--- 01_maze.cpp
+++ 01_maze.cpp
@@ -29,5 +29,42 @@ int charToInt(char ch)
 
 int main()
 {
-
+    ifstream myfile("input.txt");
+    
+    if (!myfile.is_open()) {
+        cout << "[ERROR] file open error" << endl;
+        return -1;
+    }
+    
+    char ch;
+    char maze_ch = '\0';
+    int count = 0;
+    
+    while (!myfile.eof()) {
+        myfile.get(ch);
+        
+        if (!is_integer(ch) && !is_separator(ch)) {
+            // 'T' 'X' 'b'
+            if (count > 0) {
+                for (int i = 0; i < count; i++) {
+                    if (ch == 'b') cout << ' '; // blank for b
+                    else           cout << ch;
+                }
+                count = 0;
+            }
+            else {
+                maze_ch = ch;
+            }
+        }
+        else if (is_integer(ch)) {
+            count += charToInt(ch);
+        }
+        else if (is_separator(ch)) {
+            cout << endl;
+        }
+    }
+    
+    myfile.close();
+    
+    return 0;
 }
